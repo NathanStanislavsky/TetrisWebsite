34415e8257f86778ec72ff027821436b42e26e4d
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-10-14 11:05:27 -0400

    Added hover effect

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,11 +7,17 @@ const Navbar = () => {
         <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-green-500 to-red-500">
           Tetris
         </h1>
-        <Link to="/play" className="text-white no-underline">
-          <h1 className="text-3xl">Play</h1>
+        <Link
+          to="/play"
+          className="text-white no-underline text-3xl py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-blue-500"
+        >
+          Play
         </Link>
-        <Link to="/leaderboard" className="text-white no-underline">
-          <h1 className="text-3xl">Leaderboard</h1>
+        <Link
+          to="/leaderboard"
+          className="text-white no-underline text-3xl py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-blue-500"
+        >
+          Leaderboard
         </Link>
       </div>
     </header>
