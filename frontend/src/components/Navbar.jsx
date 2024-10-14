203791f144694dd1e479e31f04968aab5847f6d8
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-10-14 10:54:38 -0400

    created navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <header className="bg-white">
-      <div className="container mx-auto max-w-[1400px] p-4 flex items-center justify-between">
-        <Link to="/" className="text-gray-800 no-underline">
-          <h1 className="font-bold text-4xl">Tetris</h1>
+      <div className="container mx-auto w-full max-w-none p-4 h-32 flex items-center justify-start space-x-8 bg-red-300">
+        <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-green-500 to-red-500">
+          Tetris
+        </h1>
+        <Link to="/play" className="text-white no-underline">
+          <h1 className="text-3xl">Play</h1>
+        </Link>
+        <Link to="/leaderboard" className="text-white no-underline">
+          <h1 className="text-3xl">Leaderboard</h1>
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className='bg-red-400 text-white'>
+    <div>
         <h2>Home</h2>    
     </div>
   )
