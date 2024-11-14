7fb697af83616a96f199713b90d75d247de14e3c
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-11-13 22:37:45 -0500

    rename and line_clearing test inital commit

---
+++ frontend/tests/TetrisGame/line_clearing.test.js
@@ -0,0 +1,24 @@
+import TetrisGame from "../../src/game/TetrisGame";
+import { TETRIS_PIECES } from "../../src/game/tetrisPieces";
+
+describe("TetrisGame Line Clearing and Scoring", () => {
+  let game;
+
+  beforeEach(() => {
+    jest.spyOn(TetrisGame.prototype, "createPiece").mockReturnValue({
+      shape: TETRIS_PIECES["I"],
+      type: "I",
+    });
+
+    // Mock canvas elements
+    const mockCanvas = document.createElement("canvas");
+    const storedPieceCanvas = document.createElement("canvas");
+    const nextPieceCanvas = document.createElement("canvas");
+
+    game = new TetrisGame(mockCanvas, storedPieceCanvas, nextPieceCanvas);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+});
