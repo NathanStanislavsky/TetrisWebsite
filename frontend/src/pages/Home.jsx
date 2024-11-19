fcccfb9d1590bcc326c2b1735959bd2c44f31c8d
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2024-11-18 20:10:52 -0500

    background for game instructions

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
-    <div>
-        <h2>Home</h2>    
+    <div className="bg-slate-800 w-9/12 h-[75vh] mx-auto my-auto flex items-center justify-center mt-8">
+      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
